4cd17bf06df7f09ecef4d07621f4c92665104f0e
Aidan Callan <callana2@newpaltz.edu>
2023-09-12 19:42:01 -0400

    Initial commit

---
+++ docs/templates/hospital-website-template/nav.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+
+
+<!-- Navbar Start -->
+<div class="container-fluid sticky-top bg-white shadow-sm">
+    <div class="container">
+        <nav class="navbar navbar-expand-lg bg-white navbar-light py-3 py-lg-0">
+            <a href="index.html" class="navbar-brand">
+                <h1 class="m-0 text-uppercase text-primary">
+                    <i class="fa fa-clinic-medical me-2"></i>
+                    Medinova
+                </h1>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav ms-auto py-0">
+                    <a href="index.php" class="nav-item nav-link active">Home</a>
+                    <a href="about.php" class="nav-item nav-link">About</a>
+                    <a href="service.php" class="nav-item nav-link">Service</a>
+                    <a href="price.php" class="nav-item nav-link">Pricing</a>
+                    <div class="nav-item dropdown">
+                        <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
+                        <div class="dropdown-menu m-0">
+                            <a href="blog.php" class="dropdown-item">Blog Grid</a>
+                            <a href="detail.php" class="dropdown-item">Blog Detail</a>
+                            <a href="team.php" class="dropdown-item">The Team</a>
+                            <a href="testimonial.php" class="dropdown-item">Testimonial</a>
+                            <a href="appointment.php" class="dropdown-item">Appointment</a>
+                            <a href="weblink.php" class="dropdown-item">Web Link</a>
+                            <a href="search.php" class="dropdown-item">Search</a>
+                        </div>
+                    </div>
+                    <a href="contact.php" class="nav-item nav-link">Contact</a>
+                </div>
+            </div>
+        </nav>
+    </div>
+</div>
+<!-- Navbar End -->
---
+++ index.php
@@ -0,0 +1,10 @@
+
+
+
+
+
+<?php
+header("Location: docs/templates/hospital-website-template/");
+	
+?>
+
