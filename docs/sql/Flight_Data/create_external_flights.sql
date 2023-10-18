b73900304d167838f6b141fdd22c6edbb1491718
lucas-28 <lpfeifer28@gmail.com>
2023-10-18 16:30:28 -0400

    External flight table

---
+++ docs/sql/Flight_Data/create_external_flights.sql
@@ -0,0 +1,14 @@
+CREATE TABLE `external_flights` (
+  `date` int DEFAULT NULL,
+  `airline` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `tail_number` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `flight_number` int DEFAULT NULL,
+  `origin` char(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `destination` char(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `dep_time` int DEFAULT NULL,
+  `delay` int DEFAULT NULL,
+  `arr_time` int DEFAULT NULL,
+  `cancelled` int DEFAULT NULL,
+  `duration` int DEFAULT NULL,
+  `distance` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
