20bc4736066e0560faaaa0482c402345d05f8abe
lucas-28 <lpfeifer28@gmail.com>
2023-10-18 16:41:59 -0400

    fix to work with phpmyadmin

--- docs/sql/Flight_Data/create_external_flights.sql
+++ docs/sql/Flight_Data/create_external_flights.sql
@@ -1,10 +1,11 @@
+USE transitwise;
 CREATE TABLE `external_flights` (
   `date` int DEFAULT NULL,
-  `airline` char(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `tail_number` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `airline` char(2) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `tail_number` char(6) CHARACTER SET utf8mb4 DEFAULT NULL,
   `flight_number` int DEFAULT NULL,
-  `origin` char(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  `destination` char(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `origin` char(3) CHARACTER SET utf8mb4 DEFAULT NULL,
+  `destination` char(3) CHARACTER SET utf8mb4 DEFAULT NULL,
   `dep_time` int DEFAULT NULL,
   `delay` int DEFAULT NULL,
   `arr_time` int DEFAULT NULL,
