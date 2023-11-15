bb844467898566154128891d79ac8de4e885aec6
Aidan Callan <callana2@newpaltz.edu>
2023-11-14 19:25:07 -0500

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -11,9 +11,8 @@ $debug = "false";
 $dbconn = mysqli_connect($server, $dbusername, $password, $db);
 
 if ($dbconn->connect_error) {
-    die('Could not connect: ' . $dbconn->connect_error);
-}
-elseif($debug == "true"){
+	die('Could not connect: ' . $dbconn->connect_error);
+} elseif ($debug == "true") {
 	echo nl2br("\nDEBUG:\n");
 	echo nl2br("3 \n 2 \n 1...");
 	echo nl2br("\n Connected successfully\n");
@@ -21,5 +20,3 @@ elseif($debug == "true"){
 
 //You can use the command below to set the default database to another db.
 //mysqli_select_db($dbconn, "webiii");
-
-?>
