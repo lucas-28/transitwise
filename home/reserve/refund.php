43b644e3882bd7639f6dbd45dd12716559df097d
stevenmac1 <stevenmacaluso@gmail.com>
2023-12-07 12:05:57 -0500

    added compliance page and edited refund page

--- home/reserve/refund.php
+++ home/reserve/refund.php
@@ -2,54 +2,12 @@
 <html>
 <head>
     <title>Refund Ticket</title>
-    <style>
-        body {
-            background-color: #cff5ff;
-            font-family: Arial, sans-serif;
-        }
-
-        h1 {
-            color: #036;
-        }
-
-        form {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-        }
-
-        label {
-            display: block;
-            margin-bottom: 5px;
-            color: #036;
-        }
-
-        input {
-            width: 100%;
-            padding: 10px;
-            margin-bottom: 15px;
-            border: 1px solid #036;
-            border-radius: 5px;
-        }
-
-        input[type="submit"] {
-            background-color: #036;
-            color: #fff;
-            border: none;
-            padding: 10px 20px;
-            cursor: pointer;
-        }
-
-        input[type="submit"]:hover {
-            background-color: #003;
-        }
-    </style>
+    <link rel="stylesheet" href="/transitwise/css/style.css">
 </head>
 <body>
     <h1>Refund Ticket</h1>
     
-    <form action="refund_process.php" method="post">
+    <form action="refundconfirm.php" method="post">
         <label for="username">Username:</label>
         <input type="text" id="username" name="username" pattern="[a-zA-Z0-9]+" required><br><br>
 
---
+++ home/reserve/refundconfirm.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Refund Confirmed!</title>
+	<link rel="stylesheet" href="/transitwise/css/style.css">
+</head>
+<body>
+    <div style="text-align: center; margin-top: 200px;">
+        <h1>Refund Confirmed!</h1>
+        <p>Your refund has been confirmed. Click the button below to return to the homepage.</p>
+        <form action="http://localhost/transitwise/home/account/userhomepage.php" method="get">
+            <button type="submit">Home</button>
+        </form>
+    </div>
+</body>
+</html>
