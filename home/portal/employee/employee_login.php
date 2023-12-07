d5ff81ec1a2a45f61335912da7e5495ba696f298
davidlin246 <144184215+davidlin246@users.noreply.github.com>
2023-12-07 17:25:14 -0500

    Employee Login
    Employee login page was revised and linked with the website

---
+++ home/portal/employee/employee_login.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Login Form in HTML and CSS | Codenhal </title>
+    <link rel="stylesheet" href="/transitwise/css/style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<header>
+    <?php include ('../../includes/topnav.php'); ?>
+</header>
+<body>
+<!--Creates the navigation side bar of links to edit/view account.-->
+<?php include ('../../includes/leftnavuser.php'); ?>
+    
+
+<body>
+    <div class="wrapper">
+        <form action ="">
+            <h1>Employee Login</h1>
+            <div class="input-box">
+                <input type="text" placeholder="Username" 
+                required>
+                <i class='bx bxs-user'></i>
+            </div>
+            <div class="input-box">
+                <input type="password" placeholder="Password" 
+                required>
+                <i class='bx bxs-lock-alt'></i>
+            </div>
+
+            <div class="remember-forget">
+                <label><input type ="checkbox"> Remeber me</label>
+                <a href="#">Forget password?</a>
+            </div>
+
+            <button type="submit" class="btn">Login</button>
+
+            <div class="register-link">
+                <p>Don't have an account? <a href="#">Register</a></p>
+            </div>
+        </form>
+    </div>
+</body>
+
+<footer>
+    <div class="footer-container">
+        <a href="/transitwise/home/portal/login.php">Transitwise Portal</a>
+        <a href="contact.html">Contact</a>
+        <a href="feedback.html">Feedback</a>
+    </div>
+</footer>
+</body>
+</html>
