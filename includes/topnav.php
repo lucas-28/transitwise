c0f58f9258f968fbff55be08bd11426c6d510fe4
Aidan Callan <callana2@newpaltz.edu>
2023-12-08 15:26:45 -0500

    Linked careers.php to top nav

--- includes/topnav.php
+++ includes/topnav.php
@@ -1,5 +1,3 @@
-
-
 <?php
 // Written by Lucas with the help of the following resources:
 // Chat GPT-4
@@ -8,7 +6,7 @@ session_start(); // Start the session
 $current_page = basename($_SERVER['PHP_SELF']); // Get the current page name
 
 
-$trips = array("reservations.php","previous-trips.php","view-tickets.php", "ticket.php");
+$trips = array("reservations.php", "previous-trips.php", "view-tickets.php", "ticket.php");
 $home = array("index.php");
 
 
@@ -22,6 +20,7 @@ $absolute_path = str_replace("\\", "/", $absolute_path); // Replace backslashes 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -32,82 +31,83 @@ $absolute_path = str_replace("\\", "/", $absolute_path); // Replace backslashes 
 
 <header>
 
-<div class="topnav">
-    <div class="web">
-    
-        <a href="/transitwise/home/index.php" <?php echo (in_array($current_page, $home)) ? "class='active'" : ""; ?>>Home</a>
-        <div class="dropdown">
-            <a href="#" class="dropbtn" >Info</a>
-            <div class="dropdown-content">
-                <a href="/transitwise/home/info/about.php">About Us</a>
-                <a href="/transitwise/home/info/FAQ.php">FAQs</a>
-                <a href="#">Careers</a>
-				<a href="/transitwise/home/info/compliance.php">Compliance</a>
+    <div class="topnav">
+        <div class="web">
+
+            <a href="/transitwise/home/index.php" <?php echo (in_array($current_page, $home)) ? "class='active'" : ""; ?>>Home</a>
+            <div class="dropdown">
+                <a href="#" class="dropbtn">Info</a>
+                <div class="dropdown-content">
+                    <a href="/transitwise/home/info/about.php">About Us</a>
+                    <a href="/transitwise/home/info/FAQ.php">FAQs</a>
+                    <a href="/transitwise/home/info/careers.php">Careers</a>
+                    <a href="/transitwise/home/info/compliance.php">Compliance</a>
+                </div>
             </div>
-        </div>
-        <div class="dropdown">
-        <a href="#" class="dropbtn" <?php echo ($current_page == "index.php") ? "class='active'" : ""; ?>>Deals</a>
-            <div class="dropdown-content">
-                <a href="#">New Deals</a>
-                <a href="#">Classics</a>
-                <a href="#">Explore</a>
+            <div class="dropdown">
+                <a href="#" class="dropbtn" <?php echo ($current_page == "index.php") ? "class='active'" : ""; ?>>Deals</a>
+                <div class="dropdown-content">
+                    <a href="#">New Deals</a>
+                    <a href="#">Classics</a>
+                    <a href="#">Explore</a>
+                </div>
             </div>
-        </div>
-        <div class="dropdown">
-        <a href="#" class="dropbtn" <?php echo ($current_page == "view-tickets.php") ? "class='active'" : ""; ?>>Trips</a>
-            <div class="dropdown-content">
-                <a href="/transitwise/home/account/reservations.php">Current Trips</a>
-                <a href="#">Past Trips</a>
+            <div class="dropdown">
+                <a href="#" class="dropbtn" <?php echo ($current_page == "view-tickets.php") ? "class='active'" : ""; ?>>Trips</a>
+                <div class="dropdown-content">
+                    <a href="/transitwise/home/account/reservations.php">Current Trips</a>
+                    <a href="#">Past Trips</a>
+                </div>
             </div>
+
         </div>
-    
-    </div>
-    
-    <!-- logo will go here
+
+        <!-- logo will go here
     <div id="logo-box">
         <img id="logo" src="/transitwise/images/logo3.png" alt="logo" width="60%" height="60%">
     </div>
     -->
-    <div class="account">
-        <?php
-        // Check if user is logged in
-        if (isset($_SESSION['email'])): 
-        ?>
-            <a href="/transitwise/home/account/logout.php" style="float:right">Sign out</a>
-            <a href="/transitwise/home/account/userhomepage.php" style="float:right">Profile</a>
-        <?php else: ?>
-            <a href="/transitwise/home/account/lp_login.php" style="float:right">Sign in</a>
-            <a href="/transitwise/home/account/register.php" style="float:right">Register</a>
-        <?php endif; ?>
+        <div class="account">
+            <?php
+            // Check if user is logged in
+            if (isset($_SESSION['email'])) :
+            ?>
+                <a href="/transitwise/home/account/logout.php" style="float:right">Sign out</a>
+                <a href="/transitwise/home/account/userhomepage.php" style="float:right">Profile</a>
+            <?php else : ?>
+                <a href="/transitwise/home/account/lp_login.php" style="float:right">Sign in</a>
+                <a href="/transitwise/home/account/register.php" style="float:right">Register</a>
+            <?php endif; ?>
+        </div>
+
+
+        <button class="nav-toggle">☰</button>
+        <div class="responsive">
+
+            <ul class="responsive-nav-list">
+                <li><a href="/transitwise/home/index.php" <?php echo (in_array($current_page, $home)) ? "class='active'" : ""; ?>>Home</a></li>
+                <li><a href="#" class="dropbtn" <?php echo ($current_page == "index.php") ? "class='active'" : ""; ?>>Deals</a></li>
+                <li><a href="#" class="dropbtn" <?php echo ($current_page == "view-tickets.php") ? "class='active'" : ""; ?>>Trips</a></li>
+                <?php
+                // Check if user is logged in
+                if (isset($_SESSION['email'])) :
+                ?>
+                    <li><a href="/transitwise/home/account/userhomepage.php">Profile</a></li>
+                    <li><a href="/transitwise/home/account/logout.php">Sign out</a></li>
+
+                <?php else : ?>
+                    <li><a href="/transitwise/home/account/register.php">Register</a></li>
+                    <li><a href="/transitwise/home/account/lp_login.php">Sign in</a></li>
+
+                <?php endif; ?>
+            </ul>
+        </div>
+        <script src="/transitwise/js/topnav.js"></script>
     </div>
-    
-
-    <button class="nav-toggle">☰</button>
-    <div class="responsive">
-        
-        <ul class="responsive-nav-list">
-        <li><a href="/transitwise/home/index.php" <?php echo (in_array($current_page, $home)) ? "class='active'" : ""; ?>>Home</a></li>
-        <li><a href="#" class="dropbtn" <?php echo ($current_page == "index.php") ? "class='active'" : ""; ?>>Deals</a></li>
-        <li><a href="#" class="dropbtn" <?php echo ($current_page == "view-tickets.php") ? "class='active'" : ""; ?>>Trips</a></li>
-        <?php
-        // Check if user is logged in
-        if (isset($_SESSION['email'])): 
-        ?>
-            <li><a href="/transitwise/home/account/userhomepage.php" >Profile</a></li>
-            <li><a href="/transitwise/home/account/logout.php" >Sign out</a></li>
-            
-        <?php else: ?>
-            <li><a href="/transitwise/home/account/register.php" >Register</a></li>
-            <li><a href="/transitwise/home/account/lp_login.php" >Sign in</a></li>
-            
-        <?php endif; ?>
-        </ul>
-    </div>
-    <script src="/transitwise/js/topnav.js"></script>
-</div>
 
 
-<!-- The main content of the page will be here -->
+    <!-- The main content of the page will be here -->
 
 </header>
+
 </html>
